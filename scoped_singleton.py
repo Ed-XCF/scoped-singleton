ee890bcc9f6dcaa93d1e2efaab8106d9444cb2dc
Ed-XCF <m.tofu@qq.com>
2022-06-24 16:28:51 +0800

    support async

--- scoped_singleton.py
+++ scoped_singleton.py
@@ -1,24 +1,64 @@
 import hashlib
 import threading
+import contextvars
 from weakref import WeakValueDictionary
 from urllib.parse import quote
+from functools import partial
+from abc import ABCMeta, abstractmethod
+from typing import Type
+
+__all__ = [
+    "thread_scoped_singleton",
+    "async_scoped_singleton",
+]
 
 
-class ScopedRegistry(threading.local):
-    registry = WeakValueDictionary()
+class Registry(metaclass=ABCMeta):
+    @abstractmethod
+    def __contains__(self, key):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __getitem__(self, item):
+        raise NotImplementedError
+
+    @abstractmethod
+    def __setitem__(self, key, value):
+        raise NotImplementedError
+
+
+class ThreadRegistry(Registry):
+    def __init__(self):
+        self.scope = threading.local()
+        self.scope.registry = WeakValueDictionary()
 
     def __contains__(self, key):
-        return key in self.registry
+        return key in self.scope.registry
 
     def __getitem__(self, item):
-        return self.registry[item]
+        return self.scope.registry[item]
 
     def __setitem__(self, key, value):
-        self.registry[key] = value
+        self.scope.registry[key] = value
 
 
-def scoped_singleton(cls):
-    _registry = ScopedRegistry()
+class AsyncRegistry(Registry):
+    def __init__(self):
+        self.scope = contextvars.ContextVar(type(self).__name__)
+        self.scope.set(WeakValueDictionary())
+
+    def __contains__(self, key):
+        return key in self.scope.get()
+
+    def __getitem__(self, item):
+        return self.scope.get()[item]
+
+    def __setitem__(self, key, value):
+        self.scope.get()[key] = value
+
+
+def scoped_singleton(registry_klass: Type[Registry], cls):
+    registry = registry_klass()
 
     def wrap(*args, **kwargs):
         quote_args = [quote(str(i)) for i in args]
@@ -27,10 +67,14 @@ def scoped_singleton(cls):
         original_key = ".".join([cls.__module__ or "", cls.__qualname__, quote_params])
         registry_key = hashlib.md5(original_key.encode()).hexdigest()
 
-        if registry_key in _registry:
-            return _registry[registry_key]
+        if registry_key in registry:
+            return registry[registry_key]
 
-        _registry[registry_key] = instance = cls(*args, **kwargs)
+        registry[registry_key] = instance = cls(*args, **kwargs)
         return instance
 
     return wrap
+
+
+thread_scoped_singleton = partial(scoped_singleton, ThreadRegistry)
+async_scoped_singleton = partial(scoped_singleton, AsyncRegistry)
--- test_scoped_singleton.py
+++ test_scoped_singleton.py
@@ -1,16 +1,16 @@
 import gc
 
-from scoped_singleton import scoped_singleton
+from scoped_singleton import *
 
 
-@scoped_singleton
-class Account:
-    def __init__(self, uuid):
-        self.uuid = uuid
-
-
-def test_scoped_singleton():
+def test_async_scoped_singleton():
     gc.collect()
+
+    @async_scoped_singleton
+    class Account:
+        def __init__(self, uuid):
+            self.uuid = uuid
+
     a, b, c = Account(uuid="123"), Account(uuid="123"), Account(uuid="124")
     assert a is b
     assert a is not c
@@ -24,7 +24,32 @@ def test_scoped_singleton():
     del b, d
     gc.collect()
     e, f = Account(uuid="456"), Account(uuid="123")
-    # f = Account(uuid="123")
+    assert f is not c
+    id_f = id(f)
+    assert id_f != id_d
+
+
+def test_thread_scoped_singleton():
+    gc.collect()
+
+    @thread_scoped_singleton
+    class Account:
+        def __init__(self, uuid):
+            self.uuid = uuid
+
+    a, b, c = Account(uuid="123"), Account(uuid="123"), Account(uuid="124")
+    assert a is b
+    assert a is not c
+    assert b is not c
+    del a
+    gc.collect()
+    d = Account(uuid="123")
+    assert d is b
+    assert d is not c
+    id_d = id(d)
+    del b, d
+    gc.collect()
+    e, f = Account(uuid="456"), Account(uuid="123")
     assert f is not c
     id_f = id(f)
     assert id_f != id_d
