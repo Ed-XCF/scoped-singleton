317e847a661d2e158f39f66b8761f7e8ab5677b2
Ed__xu__Ed <m.tofu@qq.com>
2023-05-07 22:48:55 +0800

    remove context scoped

--- test_scoped_singleton.py
+++ test_scoped_singleton.py
@@ -3,30 +3,6 @@ import gc
 from scoped_singleton import *
 
 
-def test_context_scoped_singleton():
-    gc.collect()
-
-    @context_scoped_singleton
-    class Account:
-        def __init__(self, uuid):
-            self.uuid = uuid
-
-    a, b, c = Account(uuid="123"), Account(uuid="123"), Account(uuid="124")
-    assert a is b
-    assert a is not c
-    assert b is not c
-    del a
-    gc.collect()
-    d = Account(uuid="123")
-    assert d is b
-    assert d is not c
-    id_d = id(d)
-    del b, d
-    gc.collect()
-    e, f = Account(uuid="456"), Account(uuid="123")
-    assert f is not c
-
-
 def test_thread_scoped_singleton():
     gc.collect()
 
